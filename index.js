427d10d2838fc26f4bf8fedc502472cd2851de6c
Rushi Sharma <rushisharma64@gmail.com>
2023-08-23 11:17:02 -0700

    Connection to Firebase

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./fern-stack-ee1f2-firebase-adminsdk-2pj3r-870af83c64.json")
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://fern-stack-ee1f2-default-rtdb.firebaseio.com"
+  });
+  
+
 const express = require('express');
 const app = express();
 app.get('/', function(req, res){
